518789f37a64dfa7157d08a7b1b4aca900a38752
IzanamiiDevv <rafaeloli596@gmail.com>
2024-06-16 23:00:28 +0800

    Code

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    public unsafe static void Main()
+    {
+        IntPtr ptr = Marshal.AllocHGlobal(sizeof(int) * 10);
+        int* numarr = (int*) ptr.ToPointer();
+
+        int[] numbers = { 19,2,3,4,6,19,29,3,4,5};
+
+        for(int i = 0; i < 10; i++)
+        {
+            numarr[i] = numbers[i];
+        }
+        Console.WriteLine("Hello World");
+        Console.WriteLine($"NumArr: {numarr[0]}");
+
+        Marshal.FreeHGlobal(ptr);
+    }
+}
